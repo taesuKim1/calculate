598916cf0508df0b98075fed00a3ea173c04cc85
taesuKim1 <hucet@bnpinnovation.com>
2016-03-15 15:45:49 +0900

    gg
    gg

---
+++ plus/src/main/java/com/example/bnpinnovation/plus/Plus.java
@@ -0,0 +1,12 @@
+package com.example.bnpinnovation.plus;
+
+/**
+ * Created by bnpinnovation on 2016. 3. 15..
+ */
+public class Plus {
+    public static int plus(int x, int y)
+    {
+        return x+y;
+    }
+
+}
